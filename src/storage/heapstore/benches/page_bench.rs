95b8e53c04868c8f19e182d6b0a9ea9c717b52a1
Aaron Elmore <aelmore@cs.uchicago.edu>
2025-04-17 15:35:14 -0500

    Page Bench

--- src/storage/heapstore/benches/page_bench.rs
+++ src/storage/heapstore/benches/page_bench.rs
@@ -2,21 +2,16 @@ use criterion::{criterion_group, criterion_main};
 
 use criterion::{black_box, Criterion};
 
-use common::testutil::{get_random_vec_of_byte_vec, get_rng};
-use heapstore::testutil::bench_page_insert;
+use common::testutil::get_rng;
+use heapstore::testutil::{bench_page_mixed, gen_page_bench_workload};
 
-pub fn page_benchmark(c: &mut Criterion) {
+pub fn heap_page_mixed_benchmark(c: &mut Criterion) {
     let mut rng = get_rng();
-    let to_insert = get_random_vec_of_byte_vec(&mut rng, 40, 80, 100);
-    c.bench_function("page insert medium", |b| {
-        b.iter(|| bench_page_insert(black_box(&to_insert)))
-    });
-
-    let to_insert = get_random_vec_of_byte_vec(&mut rng, 10, 350, 400);
-    c.bench_function("page insert large recs", |b| {
-        b.iter(|| bench_page_insert(black_box(&to_insert)))
+    let workload = gen_page_bench_workload(&mut rng, 5000, 20, 40);
+    c.bench_function("heap_page_mixed", |b| {
+        b.iter(|| bench_page_mixed(black_box(&workload)))
     });
 }
 
-criterion_group!(benches, page_benchmark);
+criterion_group!(benches, heap_page_mixed_benchmark);
 criterion_main!(benches);
--- src/storage/heapstore/src/testutil.rs
+++ src/storage/heapstore/src/testutil.rs
@@ -1,9 +1,87 @@
 use crate::heap_page::HeapPage;
 use crate::page::Page;
+use common::prelude::*;
+use common::testutil::*;
+use rand::rngs::SmallRng;
+use rand::Rng;
+use std::hint::black_box;
 
 pub fn bench_page_insert(vals: &[Vec<u8>]) {
     let mut p = Page::new(0);
     for i in vals {
         p.add_value(i).unwrap();
+    }
+}
+
+pub enum BenchOp {
+    Insert(Vec<u8>),
+    Delete(SlotId),
+    Update(SlotId, Vec<u8>),
+    Read(SlotId),
+    Scan,
+}
+
+pub fn gen_page_bench_workload(
+    rng: &mut SmallRng,
+    num_ops: usize,
+    min_size: usize,
+    max_size: usize,
+) -> Vec<BenchOp> {
+    let mut res = Vec::new();
+    let mut random_bytes = get_random_vec_of_byte_vec(rng, num_ops, min_size, max_size);
+    let mut expected_max_slot = 0;
+    let seed_insert = 5;
+    // Seed the first SEED_INSERT ops to be inserts
+    for _ in 0..seed_insert {
+        expected_max_slot += 1;
+        res.push(BenchOp::Insert(random_bytes.pop().unwrap()));
+    }
+    for _ in seed_insert..num_ops {
+        let op = match rng.random_range(0..100) {
+            0..20 => {
+                expected_max_slot += 1;
+                BenchOp::Insert(random_bytes.pop().unwrap())
+            }
+            20..30 => BenchOp::Delete(rng.random_range(0..expected_max_slot)),
+            30..50 => BenchOp::Update(
+                rng.random_range(0..expected_max_slot),
+                random_bytes.pop().unwrap(),
+            ),
+            50..60 => BenchOp::Scan,
+            _ => BenchOp::Read(rng.random_range(0..expected_max_slot)),
+        };
+        res.push(op);
+    }
+    res
+}
+
+pub fn bench_page_mixed(workload: &Vec<BenchOp>) {
+    let mut p = Page::new(23500);
+    p.init_heap_page();
+    for op in workload {
+        match op {
+            BenchOp::Insert(v) => {
+                let res = p.add_value(v);
+                black_box(res);
+            }
+            BenchOp::Delete(sid) => {
+                let res = p.delete_value(*sid);
+                black_box(res);
+            }
+            BenchOp::Update(sid, v) => {
+                let res = p.update_value(*sid, v);
+                black_box(res);
+            }
+            BenchOp::Read(sid) => {
+                let res = p.get_value(*sid);
+                black_box(res);
+            }
+            BenchOp::Scan => {
+                for (i, slot) in p.iter() {
+                    black_box(i);
+                    black_box(slot);
+                }
+            }
+        }
     }
 }
